1b2578d36c0a54944b76384e30ca3dbca64b0286
jlocamuz <jlocamuz@gmail.com>
2020-04-14 12:33:05 -0300

    First commit

---
+++ 59104.Locamuz.Julia/clase20200414/roman_numbers.py
@@ -0,0 +1,8 @@
+def roman_to_decimal(roman_numbers):
+    if roman_numbers == 'I':
+        return 1
+    return 2
+
+# pass lo pongo cuando no quiero poner nada
+# pero no quiero que me de error
+# return 1 no me lo imprime pero lo devuelve

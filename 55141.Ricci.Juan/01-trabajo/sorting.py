1129c243ec6da6aa5c657b54ea4bf254a50cf64b
juanlrcricci <juanjo.ricci@live.com.ar>
2020-04-16 12:01:38 -0300

    act trab 1

--- 55141.Ricci.Juan/01-trabajo/sorting.py
+++ 55141.Ricci.Juan/01-trabajo/sorting.py
@@ -2,8 +2,8 @@ class Sort():
     def BubbleSort(self, lista):
         n = len(lista)
         i = 0
-        for i in range(n):
-            for j in range(0, n-i-1):
+        for i in range(1, n):
+            for j in range(n-1):
                 if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
         return lista

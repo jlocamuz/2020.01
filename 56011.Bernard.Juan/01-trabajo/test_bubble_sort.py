0c93bdccc834e5c96517bffc0e0de5fc9623752b
JuaniBernard <juaninard@gmail.com>
2020-04-19 19:27:23 -0300

    TP1 Commit

---
+++ 56011.Bernard.Juan/01-trabajo/test_bubble_sort.py
@@ -0,0 +1,20 @@
+import unittest
+from sorting import Sort
+
+
+class TestBubbleSort(unittest.TestCase):
+    def test_sorting_1(self):
+        lista = Sort().BubbleSort([36, 71, 16, 21, 73, 9, 0, 40, 66, 7])
+        self.assertEqual(lista, [0, 7, 9, 16, 21, 36, 40, 66, 71, 73])
+
+    def test_sorting_2(self):
+        lista = Sort().BubbleSort([0, 2, 23, 4, 2, 8, 1, 25, 6, 9])
+        self.assertEqual(lista, [0, 1, 2, 2, 4, 6, 8, 9, 23, 25])
+
+    def test_sorting_3(self):
+        lista = Sort().BubbleSort([5, 0, 15, 25, 21, 35, 40, 25, 6, 9])
+        self.assertEqual(lista, [0, 5, 6, 9, 15, 21, 25, 25, 35, 40])
+
+
+if __name__ == '__main__':
+    unittest.main()
